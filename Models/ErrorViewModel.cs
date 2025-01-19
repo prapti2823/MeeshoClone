1700e60538f60729bd5d35f2327af18c949144d1
prapti <praptishah2823@gmail.com>
2025-01-19 15:44:30 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeeshoClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
